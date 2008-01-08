1978ca1a1a827b86538e197e28639c933d89dcb3
yuriks.br <yuriks.br@194990a9-7324-0410-9a02-4386df949664>
2008-01-08 01:04:50 +0000

    Enhanced: Merged in-game help (r670)
    git-svn-id: http://opentyrian.googlecode.com/svn@681 194990a9-7324-0410-9a02-4386df949664

--- enhanced/trunk/src/setup.cpp
+++ enhanced/trunk/src/setup.cpp
@@ -92,7 +92,7 @@ void JE_textMenuWait( JE_word *waitTime, bool doGamma )
 		}
 #endif
 
-		SDL_Delay(1); /* <MXD> attempt non-processor-based wait, implement a real delay later */
+		SDL_Delay(16); /* <MXD> attempt non-processor-based wait, implement a real delay later */
 		if (*waitTime > 0)
 		{
 			(*waitTime)--;
